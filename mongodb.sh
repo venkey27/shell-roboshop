b048c00b7bba777fa7aa13c1e30897abd5e44c65
venkey27 <venkey271200@gmail.com>
2026-05-28 14:17:13 -0400

    initial commit

---
+++ mongodb.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+
+USERID=$(id -u)
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $TIMESTAMP [ERROR] $R RUN T HE MONGODB SCRIPT WITH ROOT ACCESS $N " | tee -a $LOGS_FILE
+    exit 1
+fi  
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e " $TIMESTAMP [ERROR] $2 ... $R failure $N " | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e " $TIMESTAMP [INFO]  $2 ... $G is successfully completed $N " | tee -a $LOGS_FILE
+    fi
+}
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "adding mongo repo file"                       #$? -> this is the exit code of the last command
