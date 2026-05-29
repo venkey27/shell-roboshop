5dda4d6be7b5eff8a2dfdfea52fb0fcf1fb8efae
venkey27 <venkey271200@gmail.com>
2026-05-29 14:05:30 -0400

    special variables

---
+++ redis.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+
+USERID=$(id -u)
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $TIMESTAMP [ERROR] $R RUN the SCRIPT WITH ROOT ACCESS $N " | tee -a $LOGS_FILE
+    exit 1
+fi  
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e " $TIMESTAMP [ERROR] $2 ... $R failure $N " | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e " $TIMESTAMP [INFO]  $2 ... $G is successfully completed $N " | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y
+dnf module enable redis:7 -y
+dnf install redis -y 
+VALIDATE $? "installing redis:7"
+
+sed -i -e 's/127.0.01/0.0.0.0/g' -e '/proctected-mode/ c protected-mode no' /etc/redis/redis.conf 
+VALIDATE $? "allowing remote connections to redis"
+
+systemctl enable redis 
+systemctl start redis 
+VALIDATE $? "starting and enabling redis"
+ 
