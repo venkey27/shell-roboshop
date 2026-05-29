b3f96bc08635ebdde2b0c31e5cb99ba37d877443
venkey27 <venkey271200@gmail.com>
2026-05-29 14:44:22 -0400

    special variables

---
+++ mysql.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+
+USERID=$(id -u)
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $TIMESTAMP [ERROR] $R RUN the SCRIPT WITH ROOT ACCESS $N " | tee -a $LOGS_FILE
+    exit 1
+fi  
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e " $TIMESTAMP [ERROR] $2 ... $R failure $N " | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e " $TIMESTAMP [INFO]  $2 ... $G is successfully completed $N " | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y $&>> $LOGS_FILE
+VALIDATE $? "Insatalling mysql server"
+
+systemctl enable mysqld &>> $LOGS_FILE
+systemctl start mysqld &>> $LOGS_FILE
+VALIDATE $? "starting and enabling mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "setting mysql root password"
