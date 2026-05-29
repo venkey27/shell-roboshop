606bdf75629c01d04c91fd500e7cae61e90b5903
venkey27 <venkey271200@gmail.com>
2026-05-29 19:00:50 -0400

    special variables

---
+++ catalogue.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+
+USERID=$(id -u)
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $TIMESTAMP [ERROR] $R RUN the SCRIPT WITH ROOT ACCESS $N " | tee -a $LOGS_FILE
+    exit 1
+fi  
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e " $TIMESTAMP [ERROR] $2 ... $R failure $N " | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e " $TIMESTAMP [INFO]  $2 ... $G is successfully completed $N " | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "creating roboshop user"
+
+mkdir /app &>> $LOGS_FILE
+VALIDATE $? "creating application directory"
