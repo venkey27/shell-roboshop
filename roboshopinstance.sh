334a0c5a98880ec61afc95be59b8734e2d169583
venkey27 <venkey271200@gmail.com>
2026-05-30 18:03:14 -0400

    special variables

--- roboshopinstance.sh
+++ roboshopinstance.sh
@@ -38,7 +38,7 @@ do
         if [[ $INSTANCE_ID == "None" ]]; then
             echo "Launching ec2 instance : roboshop-$instance"
             INSTANCE_ID=$(aws ec2 run-instances \
-            --image-id ami-0220d79f3f480ecf5 \
+            --image-id $AMI_ID \
             --instance-type t3.micro \
             --security-groups "roboshop-common" "roboshop-$instance" \
 	        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
