f5d32c5945d6d03984a5cfb098173d197c8e418c
venkey27 <venkey271200@gmail.com>
2026-05-30 16:05:50 -0400

    special variables

--- roboshopinstance.sh
+++ roboshopinstance.sh
@@ -16,3 +16,11 @@ if [ $# -lt 2 ]; then    # $# gives the number of arguments passed to the script
     exit 1
 fi
 
+ACTION=$1
+shift # shift command is used to shift the positional parameters to the left. After this command, $1 will be the second argument, $2 will be the third argument, and so on. This allows us to easily access the instance names in the loop below.           
+
+if [ $ACTION != "create" ] && [ $ACTION != "delete" ]; then  # && is used for logical AND operation. It checks if both conditions are true. this condition checks if the ACTION variable is not equal to "create" and also not equal to "delete". If both conditions are true, it means an invalid action was specified.
+    echo -e "${R}ERROR:: Invalid action specified. Use 'create' or 'delete'.${N}"
+    echo "USAGE: $0 [create/delete] [instance1] [instance2] ..."
+    exit 1
+fi
