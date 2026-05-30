53459e4526836d4c1ecf87341fbda411a8534839
venkey27 <venkey271200@gmail.com>
2026-05-30 17:46:51 -0400

    special variables

--- roboshopinstance.sh
+++ roboshopinstance.sh
@@ -26,6 +26,7 @@ if [ $ACTION != "create" ] && [ $ACTION != "delete" ]; then  # && is used for lo
 fi
 
 get_instance_id() {
+    name=$1
     aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query
      'Reservations[0].Instances[0].InstanceId' --output text
 }
