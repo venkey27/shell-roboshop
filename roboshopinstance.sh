fa88ad1175146e10d3216dfc4c61919b50fcb5ce
venkey27 <venkey271200@gmail.com>
2026-05-30 17:49:12 -0400

    special variables

--- roboshopinstance.sh
+++ roboshopinstance.sh
@@ -28,7 +28,7 @@ fi
 get_instance_id() {
     name=$1
     aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query
-     'Reservations[0].Instances[0].InstanceId' --output text
+     "Reservations[0].Instances[0].InstanceId" --output text
 }
 
 for instance in $@
